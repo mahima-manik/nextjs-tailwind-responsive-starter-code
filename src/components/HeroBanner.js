f4e1e324871a893888a5b389717a88b29ad27b07
Mahima Manik <mahimamanik@Mahimas-MacBook-Pro-2.local>
2023-06-27 01:21:36 +0530

    Add padding and gap in Hero banner

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,13 +1,13 @@
 export default function HeroBanner() {
 
   return (
-    <div className="flex flex-col-reverse sm:flex-row py-5 gap-10 bg-emerald-500">
+    <div className="flex flex-col-reverse sm:flex-row gap-5 sm:gap-10 px-2 py-2 sm:px-5 sm:py-5 bg-emerald-500">
         <div className="flex flex-col flex-1 outline-dashed outline-1">
             <h1>Responsive Tailwind Starter code</h1>
             <p>Kickstart your web projects swiftly and efficiently using responsive component in flexbox for all screen sizes.</p>
             <p>View now button</p>
         </div>
-        <div className="flex flex-col flex-1 items-center justify-center">
+        <div className="flex flex-col flex-1">
             <div className="min-h-full min-w-full outline-dashed outline-1">
                 Hero Image goes here
             </div>
